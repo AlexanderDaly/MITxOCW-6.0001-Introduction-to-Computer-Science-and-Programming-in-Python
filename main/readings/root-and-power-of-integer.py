222efaae072048b5e93bb0d9aed23460e5cb6021
miserable ape <31634797+AlexanderDaly@users.noreply.github.com>
2023-01-06 20:17:04 -0500

    Update root-and-power-of-integer.py

--- main/readings/root-and-power-of-integer.py
+++ main/readings/root-and-power-of-integer.py
@@ -19,11 +19,3 @@ if root == 0 and pwr == 0:
 else:
   print("root =", root)
   print("pwr =", pwr)
-
-#This program first gets an integer from the user using the input() function. It then initializes the variables root and pwr to 0.
-#The program then uses two nested for loops to try all possible values of root and pwr in the range 1 to 5. 
-#For each pair of values, it checks if root**pwr is equal to the integer entered by the user. 
-#If it is, it sets root and pwr to the values that satisfy the condition and breaks out of the loops.
-#Finally, the program checks if root and pwr are still equal to 0. 
-#If they are, it means that no such pair of integers was found, and it prints a message to that effect.
-#If root and pwr have been set to non-zero values, it means that a pair of integers was found, and it prints the values of root and pwr.
